f4e265a86f393ebe584153d33266f54778ffb4a1
Eshan-Agarwal <eshanagarwal12@gmail.com>
2020-11-30 09:48:34 +0530

    Update Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+
+import React from 'react';
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 
-function App() {
-  return <div>
-    <HomePage/>
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
   </div>
+);
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop/hats' component={HatsPage} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
